23a95875c3a79567c5b9b40c76fda87380e17804
SUMBAL RIZWAN <164493746+snazcoder2323@users.noreply.github.com>
2024-03-31 03:05:30 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWELCOME TO\'CODE WITH SNAZ'\-CLI PROJECT(TO DO LIST)\n")
+
+let todos: string[] = [""]
+let loop = true;
+
+while(loop){
+    const answer:{
+        TODO: string,
+        addmore:boolean
+    } = await inquirer.prompt([
+        {
+            type:"input",
+            name:"TODO",
+            message: "What do you want to add in your todo? "
+        },
+        {
+            type:"confirm",
+            name:"addmore",
+            message:"Do You Want To Add More Todo?",
+            default: false
+        }
+    ])
+const{TODO,addmore} = answer;
+console.log(answer)
+loop = addmore 
+if(TODO){
+    todos.push(TODO)
+}else {
+    console.log("Kindly add valid input")
+}
+// console.log(todos)
+if(todos.length >0){
+    console.log("Your TODO list:")
+    todos.forEach(todo => {
+        // console.log(todo)
+    });
+}else{
+    console.log("Not TODO found")
+}
+}
